01128743ed59cb3a3c501d22c9154b55125de862
Abdallah Muhammed <abdallahiifox@gmail.com>
2023-06-10 02:47:19 +0300

    add:Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import RootLayout from "./components/Layout/RootLayout";
 import Navigationbar from "./components/Navigation/Navigationbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <RootLayout>
       <Navigationbar />
-        <Outlet />
+      <Outlet />
+      <Footer />
     </RootLayout>
   );
 }
