5d15aa6b8fcfbd55014920c6b5d9259fc663d314
Abdallah Muhammed <abdallahiifox@gmail.com>
2023-06-13 03:31:03 +0300

    update:freelancer register form low code with Error message comopnent from formik

--- src/components/AuthComponents/FreelancerRegister.jsx
+++ src/components/AuthComponents/FreelancerRegister.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Formik, useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { freelancerSchemaValidation } from "../../Schemas/FreelancerRegisterationSchema";
 
 const feilds = {
@@ -18,7 +18,7 @@ const feilds = {
 function FreelancerRegister() {
   const handelRegisterSubmit = (values, { resetForm }) => {
     console.log("Done!!");
-    resetForm()
+    resetForm();
   };
   return (
     <div
@@ -42,158 +42,137 @@ function FreelancerRegister() {
             validationSchema={freelancerSchemaValidation}
             onSubmit={handelRegisterSubmit}
           >
-            {({
-              values,
-              errors,
-              handleSubmit,
-              handleChange,
-              handleBlur,
-              touched,
-              isSubmitting,
-            }) => (
-              <form
-                className="row g-3 col-lg-6 col-md-12"
-                onSubmit={handleSubmit}
-              >
+            {({ values, touched, isSubmitting }) => (
+              <Form className="row g-3 col-lg-6 col-md-12">
                 <div className=" col-md-6">
                   <label htmlFor="firstName" className="form-label">
                     First Name <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="text"
                     className="form-control"
                     id="firstName"
                     placeholder="firstName"
                     name="firstName"
-                    onChange={handleChange}
-                    value={values.firstName}
-                    onBlur={handleBlur}
                   />
-                  {errors.firstName && touched.firstName && (
-                    <p className="text-danger mt-2">{errors.firstName}</p>
-                  )}
+                  <ErrorMessage
+                    name="firstName"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className=" col-md-6">
                   <label htmlFor="lastName" className="form-label">
                     Last Name <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="text"
                     className="form-control"
                     id="lastName"
                     placeholder="lastName"
                     name="lastName"
-                    onChange={handleChange}
-                    value={values.lastName}
-                    onBlur={handleBlur}
                   />
-                  {errors.lastName && touched.lastName && (
-                    <p className="text-danger mt-2">{errors.lastName}</p>
-                  )}
+                  <ErrorMessage
+                    name="lastName"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className=" col-md-6">
                   <label htmlFor="username" className="form-label">
                     username <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="text"
                     className="form-control"
                     id="username"
                     placeholder="username"
                     name="username"
-                    onChange={handleChange}
-                    value={values.username}
-                    onBlur={handleBlur}
                   />
-                  {errors.username && touched.username && (
-                    <p className="text-danger mt-2">{errors.username}</p>
-                  )}
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className=" col-md-6">
                   <label htmlFor="jobTitle" className="form-label">
                     job Title <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="text"
                     className="form-control"
                     id="jobTitle"
                     placeholder="ex - Web Developer"
                     name="jobTitle"
-                    onChange={handleChange}
-                    value={values.jobTitle}
-                    onBlur={handleBlur}
                   />
-                  {errors.jobTitle && touched.jobTitle && (
-                    <p className="text-danger mt-2">{errors.jobTitle}</p>
-                  )}
+                  <ErrorMessage
+                    name="jobTitle"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className=" col-md-12">
                   <label htmlFor="inputEmail4" className="form-label">
                     Email <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="email"
                     className="form-control"
                     id="inputEmail4"
                     placeholder="Email"
                     name="email"
-                    onChange={handleChange}
-                    value={values.email}
-                    onBlur={handleBlur}
                   />
-                  {errors.email && touched.email && (
-                    <p className="text-danger mt-2">{errors.email}</p>
-                  )}
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className=" col-md-12">
                   <label htmlFor="phoneNumber" className="form-label">
                     Phone Number <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="text"
                     className="form-control"
                     id="phoneNumber"
                     placeholder="PhoneNumber"
                     name="phoneNumber"
-                    onChange={handleChange}
-                    value={values.phoneNumber}
-                    onBlur={handleBlur}
                   />
-                  {errors.phoneNumber && touched.phoneNumber && (
-                    <p className="text-danger mt-2">{errors.phoneNumber}</p>
-                  )}
+                  <ErrorMessage
+                    name="phoneNumber"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className="col-md-6">
                   <label htmlFor="inputPassword4" className="form-label">
                     Password <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="password"
                     className="form-control"
                     id="inputPassword4"
                     placeholder="password"
                     name="password"
-                    onChange={handleChange}
-                    value={values.password}
-                    onBlur={handleBlur}
                   />
-                  {errors.password && touched.password && (
-                    <p className="text-danger mt-2">{errors.password}</p>
-                  )}
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className="col-md-6">
                   <label htmlFor="inputPassword5" className="form-label">
                     Confirm Password <span className="text-danger">*</span>
                   </label>
-                  <input
+                  <Field
                     type="password"
                     className="form-control"
                     id="inputPassword5"
                     placeholder="confirmPassword"
                     name="confirmPassword"
-                    onChange={handleChange}
-                    value={values.confirmPassword}
-                    onBlur={handleBlur}
                   />
                   {values.password !== values.confirmPassword &&
                     touched.confirmPassword && (
@@ -204,22 +183,22 @@ function FreelancerRegister() {
                   <label htmlFor="categoryId" className="form-label">
                     Category <span className="text-danger">*</span>
                   </label>
-                  <select
+                  <Field
                     className="form-select"
                     aria-label="Default select example"
                     name="categoryId"
-                    onChange={handleChange}
-                    value={values.categoryId}
-                    onBlur={handleBlur}
+                    as="select"
                   >
                     <option>Choose Category</option>
                     <option value={1}>One</option>
                     <option value={2}>Two</option>
                     <option value={3}>Three</option>
-                  </select>
-                  {errors.categoryId && touched.categoryId && (
-                    <p className="text-danger mt-2">{errors.categoryId}</p>
-                  )}
+                  </Field>
+                  <ErrorMessage
+                    name="categoryId"
+                    component="div"
+                    className="text-danger"
+                  />
                 </div>
                 <div className="col-12  mx-auto">
                   <button type="submit" className="btn btn-primary">
@@ -229,7 +208,7 @@ function FreelancerRegister() {
                 <span>
                   Have you account <Link to="/login">Log In</Link>
                 </span>
-              </form>
+              </Form>
             )}
           </Formik>
         </div>
