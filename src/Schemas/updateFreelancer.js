b786bf9a8098cc10da1783d158744082ad2d9c61
Abdallah Muhammed <abdallahiifox@gmail.com>
2023-06-15 00:32:09 +0300

    fix:schema validation for update freelancer

--- src/Schemas/updateFreelancer.js
+++ src/Schemas/updateFreelancer.js
@@ -5,5 +5,5 @@ export const updateFreelanceSchema = Yup.object().shape({
     firstName:Yup.string().required('Required').min(4,'must be more than 4 characters'),
     lastName:Yup.string().required('Required').min(4,'must be more than 4 characters'),
     jobTitle:Yup.string().required('Required'),
-    description:Yup.string.required('Required').min(10,'must be more than 10 character')
+    description:Yup.string().required('Required').min(10,'must be more than 10 character')
 })
