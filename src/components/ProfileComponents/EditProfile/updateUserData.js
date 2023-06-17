5867cd92d0c83d1daa610b12fff0d32cf2c7c38b
Abdallah Muhammed <abdallahiifox@gmail.com>
2023-06-17 13:54:03 +0300

    fix:indentation part 2

--- src/components/ProfileComponents/EditProfile/updateUserData.js
+++ src/components/ProfileComponents/EditProfile/updateUserData.js
@@ -1,32 +1,34 @@
 import axios from "axios";
 import swal from "sweetalert";
-export async function updateUserData(role,id,values,token){
-    if(role === 'freelancer'){
-        console.log('dasdas')
-        try {
-            const freelancerUpdated = await axios.patch(`${import.meta.env.VITE_API_URL}/freelancers/${id}`,JSON.stringify(values),{
-                headers:{
-                    "Content-Type":"application/json",
-                   Authorization:token
-                }
-            })
-
-            const confirm = await swal({
-                title:'Updated Done',
-                icon:'success',
-            })
-
-            if(confirm){
-                window.location = `/profile/statistics/${id}`
-            }
-        } catch (error) {
-            swal({
-                title:'fild to update',
-                icon:'error'
-            })
+export async function updateUserData(role, id, values, token) {
+  if (role === "freelancer") {
+    console.log("dasdas");
+    try {
+      const freelancerUpdated = await axios.patch(
+        `${import.meta.env.VITE_API_URL}/freelancers/${id}`,
+        JSON.stringify(values),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+          },
         }
+      );
 
-    }else{
+      const confirm = await swal({
+        title: "Updated Done",
+        icon: "success",
+      });
 
+      if (confirm) {
+        window.location = `/profile/statistics/${id}`;
+      }
+    } catch (error) {
+      swal({
+        title: "fild to update",
+        icon: "error",
+      });
     }
+  } else {
+  }
 }
