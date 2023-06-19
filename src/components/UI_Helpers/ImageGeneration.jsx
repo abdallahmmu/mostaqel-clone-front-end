a93b7b12558590ba0996ab619dc04196ecbc52d4
Abdallah Muhammed <abdallahiifox@gmail.com>
2023-06-19 18:26:48 +0300

    update:image gerneration component

--- src/components/UI_Helpers/ImageGeneration.jsx
+++ src/components/UI_Helpers/ImageGeneration.jsx
@@ -7,7 +7,7 @@ function ImageGeneration({firstName,LastName,role, avatarUrl,avatar}) {
     <div className='image-generation mx-auto' role={role? role : ''}>
        {firstName && LastName && <span>{generateNames(firstName,LastName)}</span>}
        {avatarUrl && <img src={avatarUrl} className="rounded-circle" width={84} height={84} alt="Profile Pic" />}
-       {avatar && <img src={`http://localhost:3300/${avatar}`} className="rounded-circle" width={84} height={84} alt="Profile Pic" />}
+       {avatar && <img src={`${avatar}`} className="rounded-circle" width={84} height={84} alt="Profile Pic" />}
     </div>
   )
 }
