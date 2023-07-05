68ba03b02aec8871ca784791a5de39538ae2b327
Abdallah Muhammed <abdallahiifox@gmail.com>
2023-07-05 11:34:47 +0300

    add: edit skills

--- src/components/ProfileComponents/EditProfile/getUserByIdLoader.js
+++ src/components/ProfileComponents/EditProfile/getUserByIdLoader.js
@@ -6,8 +6,21 @@ export async function getUserByIdLoader({params}){
     if(!userId) return redirect('/')
     if(userData.role === 'freelancer'){
         try {
-            const freelancerData = await axios.get(`${import.meta.env.VITE_API_URL}/freelancers/${userId}`)
-            return freelancerData.data
+            const [freelancerData,getAllSkills] = await Promise.all([ axios.get(`${import.meta.env.VITE_API_URL}/freelancers/${userId}`),axios.get(`${import.meta.env.VITE_API_URL}/skills`)])
+
+            let skills = getAllSkills.data.results.reduce((acc,prevItem)=>{
+                acc.push({
+                    value:prevItem.name,
+                    lable:prevItem.name,
+                    id:prevItem._id,
+                })
+               return acc
+            },[])
+            
+            return {
+                data:freelancerData.data.data,
+                skills
+            }
         } catch (error) {
             return redirect('/')
         }
