12bd379b9375e781e1fbb1987da9c593ea0347b6
Mr Robot <hemdan219@gmail.com>
2023-07-11 06:57:05 +0300

    pulling

--- src/components/Chats/ChatsPage.jsx
+++ src/components/Chats/ChatsPage.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import styles from "./ChatPage.module.css";
 import ChatItem from "./ChatItem.jsx";
 import ChatSearch from "./ChatSearch.jsx";
+import { fetchAllChats } from "./helpers.js";
 
 const ChatsPage = () => {
+  const [chats, setChats] = useState([]);
+  const token = useSelector((state) => state.authSlice.userData?.token);
+  useEffect(() => {
+    fetchAllChats(token, setChats);
+  }, []);
   return (
     <div className={`pt-5 ${styles.chat_page}`}>
       <div className="container">
@@ -13,7 +20,9 @@ const ChatsPage = () => {
         <div className="row">
           <ChatSearch />
           <div className="col-md-9">
-            <ChatItem />
+            {chats.map((chat) => {
+              return <ChatItem key={Math.random()} chat={chat} />;
+            })}
           </div>
         </div>
       </div>
